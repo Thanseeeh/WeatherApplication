3cefcf83127b2dc5c73e6a4bd54eb33618f744f5
Thanseeh yaseen <yaseenkdr27@gmail.com>
2023-11-01 06:48:38 +0530

    Search Component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8b272ac2afmsh4fb9fe336b022a4p12cb14jsna137ee12df55',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
